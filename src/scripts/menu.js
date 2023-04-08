fb7720f7d582236cc1d88bb1abef306ef6f1ad03
iamverg <dev01.dash@gmail.com>
2023-04-08 16:38:21 +0200

    improved hamburger menu, accessability and header styling

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,15 @@
 
-      document.querySelector('.hamburger').addEventListener('click', () => {
-        document.querySelector('.nav-links').classList.toggle('expanded');
+      document.querySelector('.hamburger').addEventListener('click', (e) => {
+         document.querySelector('.nav-links').classList.toggle('expanded');
+        e.currentTarget.classList.toggle('hamburger--expanded');
+        console.log(e.currentTarget.getAttribute('aria-expanded'));
+        if(e.currentTarget.getAttribute('aria-expanded') === 'true') {
+          e.currentTarget.setAttribute('aria-expanded',false);
+          e.currentTarget.setAttribute('aria-label', 'Open menu');
+        }
+        else {
+          e.currentTarget.setAttribute('aria-expanded', true);
+          e.currentTarget.setAttribute('aria-label', 'Close menu');
+        }
+
       });
